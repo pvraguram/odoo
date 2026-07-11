df54647e7fc33901b0fb7960b12b13d6945f43c8
P V RAGURAM <pvraguram1407@gmail.com>
2026-07-11 18:28:17 +0530

    Delete student_management directory

--- student_management/models/student.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class Student(models.Model):
-    _name = "student.management"
-    _description = "Student"
-
-    name = fields.Char(string="Student Name", required=True)
